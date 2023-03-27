264319e738f13c1600a4bbcb4cd7d42097dda327
dhfflqldk <ofong4@my.bcit.ca>
2023-03-27 14:24:33 -0700

    main

---
+++ data/matrix.js
@@ -0,0 +1,33 @@
+export const matrix = {
+    courses: [
+        {
+            term: 1,
+            code: "COMM 1118",
+            title: "Communication for Digital Design and Development 1",
+            credits: 3.0,
+            description: "description 1"
+        },
+        {
+            term: 2,
+            code: "COMM 2218",
+            title: "Communication for Digital Design and Development 2",
+            credits: 4.0,
+            description: "description 1"
+        },
+        {
+            term: 3,
+            code: "COMM 3170",
+            title: "Front-End Web Development 2",
+            credits: 3.0,
+            description: "description 1"
+        },
+        {
+            term: 4,
+            code: "BLAW 3205",
+            title: "Internet and IT Law",
+            credits: 3.0,
+            description: "description 4"
+        }
+
+    ]
+}
